37e2fee393031579f4a498643896e1edbc2926e4
jacobsanchezmoreira <47833441+jacobsanchezmoreira@users.noreply.github.com>
2021-12-22 10:23:44 +0100

    Update

---
+++ Prueba2 Trabajo/Prueba2 Trabajo/Prueba2 Trabajo.srcs/sources_1/new/Display.vhd
@@ -0,0 +1,33 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity top is
+    port
+    (
+        code    : in  std_logic_vector(3 downto 0);
+        digsel  : in  std_logic_vector(3 downto 0);
+        AN : out std_logic_vector(3 downto 0); -- Digcontrol
+        segment : out std_logic_vector(6 downto 0)
+     );
+end top;
+
+architecture structural of top is
+    component decoder
+    port
+    (
+        code : in  std_logic_vector(3 downto 0);
+        led  : out std_logic_vector(6 downto 0)
+    );
+    end component;
+    
+begin
+    Inst_decoder: decoder port map
+    (
+        code => code,
+        led => segment
+    );
+    digctrl <= not digsel;
+    
+end structural;
