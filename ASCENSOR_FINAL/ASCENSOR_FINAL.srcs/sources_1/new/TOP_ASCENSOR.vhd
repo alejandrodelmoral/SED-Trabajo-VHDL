5aa76df2a75bcd19f00192a97bb9077976eb01a7
alejandro.moral.lapresta <alejandro.moral.lapresta@alumnos.upm.es>
2022-01-18 00:31:27 +0100

    Mejoras en TOP_ASCENSOR

--- ASCENSOR_FINAL/ASCENSOR_FINAL.srcs/sources_1/new/TOP_ASCENSOR.vhd
+++ ASCENSOR_FINAL/ASCENSOR_FINAL.srcs/sources_1/new/TOP_ASCENSOR.vhd
@@ -4,36 +4,34 @@ use ieee.std_logic_1164.all;
 entity TOP_ASCENSOR is
     port
         ( 
-            CLK : in std_logic;
-            RESET: in std_logic;
-            PISO_DESTINO : in std_logic_vector (3 downto 0);
+            CLK            : in  std_logic;
+            RESET          : in  std_logic;
+            PISO_DESTINO   : in  std_logic_vector (3 downto 0);
             PUERTA_ABIERTA : out std_logic_vector (1 downto 0);
             DISPLAY_NUMERO : out std_logic_vector (6 downto 0);
             DISPLAY_SELECT : out std_logic_vector (7 downto 0);
-            MOTOR_PUERTA : out std_logic_vector (1 downto 0)
+            MOTOR_PUERTA   : out std_logic_vector (1 downto 0)
      );
 end entity;
 
 architecture structural of TOP_ASCENSOR is
-    signal clk_sinc : std_logic;
+    signal clk_sinc    : std_logic;
     signal piso_actual : std_logic_vector(1 downto 0);
-    signal piso_d : std_logic_vector (1 downto 0);
-    signal mov : std_logic;
+    signal piso_d      : std_logic_vector (1 downto 0);
+    signal motor       : std_logic;
 
 component FSM_MASTER is
     port
     (
-        CLK : in std_logic;
-        RESET : in std_logic;
-        DESTINO : in std_logic_vector (1 downto 0);
+        CLK         : in  std_logic;
+        RESET       : in  std_logic;
+        DESTINO     : in  std_logic_vector (1 downto 0);
         DESTINO_OUT : out std_logic_vector (1 downto 0);
-        PUERTA: out std_logic_vector (1 downto 0);
-        MOTOR: out std_logic_vector(1 downto 0);
-        FLAG_MOTOR: out std_logic
+        PUERTA      : out std_logic_vector (1 downto 0);
+        MOTOR       : out std_logic_vector(1 downto 0);
+        FLAG_MOTOR  : out std_logic
     );
 end component;
-
-     
 
 component FSM_ESCLAVO is
     port
@@ -46,7 +44,6 @@ component FSM_ESCLAVO is
     );
 end component;
           
-
 component DIVISOR_FRECUENCIA_RELOJ is
     generic
     (
@@ -60,7 +57,6 @@ component DIVISOR_FRECUENCIA_RELOJ is
     ); 
 end component;
      
-
 component DECODIFICADOR_BCD is
     port
     (
@@ -71,36 +67,37 @@ component DECODIFICADOR_BCD is
 end component;
       
 begin
-     inst_Maquina_Estado: FSM_MASTER port map(
-        clk => clk_sinc,
-        reset => reset,
-        destino => piso_d,
-        destino_out => piso_actual,
-        puerta => puerta_abierta,
-        motor => motor_puerta,
-        flag_motor => mov
-     );
+    inst_Maquina_Estado: FSM_MASTER port map
+    (
+        CLK         => clk_sinc,
+        RESET       => RESET,
+        DESTINO     => piso_d,
+        DESTINO_OUT => piso_actual,
+        PUERTA      => PUERTA_ABIERTA,
+        MOTOR       => MOTOR_PUERTA,
+        FLAG_MOTOR  => motor
+    );
 
     inst_Maquina_Estado1: FSM_ESCLAVO port map
     (
-        clk => clk,
-        reset => reset,
-        SWITCH => piso_destino,
-        DESTINO => piso_d,
-        FLAG_MOTOR => mov
-      );
+        CLK        => CLK,
+        RESET      => RESET,
+        SWITCH     => PISO_DESTINO,
+        DESTINO    => piso_d,
+        FLAG_MOTOR => motor
+    );
 
     inst_Maquina_Estado2: DIVISOR_FRECUENCIA_RELOJ port map
     (
         CLK_100MHz => CLK,
-        RESET => RESET,
-        CLK_100Hz => clk_sinc
+        RESET      => RESET,
+        CLK_100Hz  => clk_sinc
     );
 
     inst_Maquina_Estado3: DECODIFICADOR_BCD port map
     (
-        LED => DISPLAY_NUMERO,
-        CODIGO => piso_actual,
+        LED     => DISPLAY_NUMERO,
+        CODIGO  => piso_actual,
         DISPLAY => DISPLAY_SELECT
     );
 end architecture structural;
