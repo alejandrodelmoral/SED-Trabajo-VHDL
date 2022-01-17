dd71c66e32a8d41af76ba300ee25e71476771e2e
weizheng13 <wei.zheng13@alumnos.upm.es>
2022-01-17 11:51:11 +0100

    MAQUINA DE ESTADO

--- ASCENSOR_FINAL/ASCENSOR_FINAL.srcs/sources_1/new/FSM_ESCLAVO.vhd
+++ ASCENSOR_FINAL/ASCENSOR_FINAL.srcs/sources_1/new/FSM_ESCLAVO.vhd
@@ -1,43 +1,92 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 17.01.2022 10:33:11
--- Design Name: 
--- Module Name: FSM_ESCLAVO - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity Pulse_register is
+    Port ( CLK : in std_logic;
+           RESET : in std_logic;
+           SWITCH : in std_logic_vector(3 downto 0);
+           DESTINO: out std_logic_vector(1 downto 0);
+           FLAG_MOTOR : in std_logic := '0'
+    );
+end Pulse_register;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity FSM_ESCLAVO is
---  Port ( );
-end FSM_ESCLAVO;
-
-architecture Behavioral of FSM_ESCLAVO is
-
+architecture Behavioral of Pulse_register is
+    type STATE_type is (S0, S1, S2, S3);
+    signal STATE, SIGUIENTE_STATE : STATE_type;
+    
 begin
 
+    SYNC_PROC: process (CLK)
+    begin
+        if rising_edge(CLK) then
+            if (RESET = '0') then
+                STATE <= S0;
+            elsif FLAG_MOTOR = '0' then
+                STATE <= SIGUIENTE_STATE;
+            end if;
+        end if;
+    end process;
 
+    OUTPUT_DECODE: process (STATE)
+    begin
+        case (STATE) is
+            when S0 => DESTINO <= "00";
+            when S1 => DESTINO <= "01";
+            when S2 => DESTINO <= "10";
+            when S3 => DESTINO <= "11";
+            when others => DESTINO <= "00";
+        end case;
+    end process;
+    
+    NEXT_STATE_DECODE: process (STATE, SWITCH)
+    begin
+        SIGUIENTE_STATE <= STATE;
+        case (STATE) is
+            
+            when S0 =>
+                if(SWITCH = "0001") then 
+                    SIGUIENTE_STATE <= S0;
+                elsif(SWITCH = "0010") then
+                    SIGUIENTE_STATE <= S1;
+                elsif(SWITCH = "0100") then
+                    SIGUIENTE_STATE <= S2;
+                elsif(SWITCH = "1000") then
+                    SIGUIENTE_STATE <= S3;
+                end if;
+                
+            when S1 =>
+                if(SWITCH = "0001") then 
+                    SIGUIENTE_STATE <= S0;
+                elsif(SWITCH = "0010") then
+                    SIGUIENTE_STATE <= S1;
+                elsif(SWITCH = "0100") then
+                    SIGUIENTE_STATE <= S2;
+                elsif(SWITCH = "1000") then
+                    SIGUIENTE_STATE <= S3;
+                end if;
+
+            when S2 =>
+                if(SWITCH = "0001") then 
+                    SIGUIENTE_STATE <= S0;
+                elsif(SWITCH = "0010") then
+                    SIGUIENTE_STATE <= S1;
+                elsif(SWITCH = "0100") then
+                    SIGUIENTE_STATE <= S2;
+                elsif(SWITCH = "1000") then
+                    SIGUIENTE_STATE <= S3;
+                end if;
+            
+            when S3 =>
+                if(SWITCH = "0001") then 
+                    SIGUIENTE_STATE <= S0;
+                elsif(SWITCH = "0010") then
+                    SIGUIENTE_STATE <= S1;
+                elsif(SWITCH = "0100") then
+                    SIGUIENTE_STATE <= S2;
+                elsif(SWITCH = "1000") then
+                    SIGUIENTE_STATE <= S3;
+                end if;
+        end case;
+    end process;
+    
 end Behavioral;
