4e38055247c61fa8c0a067a09dfd809f2e3671bd
jacobsanchezmoreira <47833441+jacobsanchezmoreira@users.noreply.github.com>
2022-01-18 00:48:14 +0100

    Comentar divisor frecuencia

--- ASCENSOR_FINAL/ASCENSOR_FINAL.srcs/sources_1/new/DIVISOR_FRECUENCIA_RELOJ.vhd
+++ ASCENSOR_FINAL/ASCENSOR_FINAL.srcs/sources_1/new/DIVISOR_FRECUENCIA_RELOJ.vhd
@@ -5,32 +5,33 @@ use ieee.numeric_std.all;
 entity DIVISOR_FRECUENCIA_RELOJ is
     generic
     (
-        FREC : integer := 100000000
+        FREC : integer := 100000000     -- Frecuencia de reloj
     );
     port
     (
-        CLK_100MHz : in  std_logic;
-        RESET      : in  std_logic;
-        CLK_100Hz  : out std_logic
+        CLK_100MHz : in  std_logic;     -- Reloj a 100 Mhz
+        RESET      : in  std_logic;     -- Reset
+        CLK_100Hz  : out std_logic      -- Reloj a 0.5 Hz
     ); 
 end entity;
 
 architecture behavioral of DIVISOR_FRECUENCIA_RELOJ is
-    signal CONTADOR : integer := 1;
-    signal CLK_TEMP : std_logic;
+    signal CONTADOR : integer := 1;         -- Contador
+    signal CLK_TEMP : std_logic;            -- Salida de pulso de 100Hz
+    
 begin
     process(CLK_100MHz, RESET)
     begin
-        if RESET = '0' then
+        if RESET = '0' then                 -- Reset con prioridad
             CONTADOR <= 1;
             CLK_TEMP <= '0';
-        elsif rising_edge(CLK_100MHz) then
+        elsif rising_edge(CLK_100MHz) then  -- Cuando hay un flanco ascendente de reloj
             CONTADOR <= CONTADOR + 1;
-            if CONTADOR = FREC then
-                CLK_TEMP <= not CLK_TEMP;
+            if CONTADOR = FREC then         -- Una vez el reloj cuente 100M de pulsos
+                CLK_TEMP <= not CLK_TEMP;   -- Cambia de nivel el pulso cada 1 seg
                 CONTADOR <= 1;
             end if;
         end if;
-        CLK_100Hz <= CLK_TEMP;
+        CLK_100Hz <= CLK_TEMP;              -- Se genera el pulso de reloj
      end process;
 end architecture behavioral;
