f9856d355ed5e16fdfbe1d7fae0cd5d01cbd98be
jacobsanchezmoreira <47833441+jacobsanchezmoreira@users.noreply.github.com>
2022-01-18 01:04:11 +0100

    Comentar Master

--- ASCENSOR_FINAL/ASCENSOR_FINAL.srcs/sources_1/new/FSM_MASTER.vhd
+++ ASCENSOR_FINAL/ASCENSOR_FINAL.srcs/sources_1/new/FSM_MASTER.vhd
@@ -4,41 +4,46 @@ use ieee.std_logic_1164.all;
 entity FSM_MASTER is
     port
     (
-        CLK         : in  std_logic;
-        RESET       : in  std_logic;
-        DESTINO     : in  std_logic_vector (1 downto 0);
-        DESTINO_OUT : out std_logic_vector (1 downto 0);
-        PUERTA      : out std_logic_vector (1 downto 0);
-        MOTOR       : out std_logic_vector (1 downto 0);
-        FLAG_MOTOR  : out std_logic
+        CLK         : in  std_logic;                            -- Reloj
+        RESET       : in  std_logic;                            -- Reset
+        DESTINO     : in  std_logic_vector (1 downto 0);        -- Planta destino
+        DESTINO_OUT : out std_logic_vector (1 downto 0);        -- Planta destino salida
+        PUERTA      : out std_logic_vector (1 downto 0);        -- Estado de la puerta, abierta (01) o cerrada (10)
+        MOTOR       : out std_logic_vector (1 downto 0);        -- Estado del motor, en marcha (01 (subiendo) | 10 (bajando)) o parado (00)
+        FLAG_MOTOR  : out std_logic                             -- Flag del motor, marca el comienzo y final del ascensor
     );
 end entity;
 
 architecture behavioral of FSM_MASTER is
-    type STATE_type is (S0, S1, S2, S3);
-    signal STATE: state_type;
-    signal SIGUIENTE_STATE : state_type;
-    signal PISO_TEMP : std_logic_vector (1 downto 0);
+    type STATE_type is (S0, S1, S2, S3);                        -- Estados
+    signal STATE: state_type;                                   -- Estado actual
+    signal SIGUIENTE_STATE : state_type;                        -- Estado siguiente
+    signal PISO_TEMP : std_logic_vector (1 downto 0);           -- Salida de planta
     
 begin
 
- SYNC_PROC: process (CLK, RESET)
+    SYNC_PROC: process (CLK, RESET)
     begin
-        if (RESET = '0') then
-                STATE <= S0;
-        elsif rising_edge(CLK) then
-            STATE <= SIGUIENTE_STATE;
+        if (RESET = '0') then               -- Reset con prioridad
+                STATE <= S0;                -- Estado inicial
+        elsif rising_edge(CLK) then         -- En los flancos de reloj
+            STATE <= SIGUIENTE_STATE;       -- Cambia de estado
         end if;
     end process;
     
     OUTPUT_DECODE: process (STATE)
     begin
-        case (STATE) is
-            when S0 => DESTINO_OUT <= "00";
-            when S1 => DESTINO_OUT <= "01";
-            when S2 => DESTINO_OUT <= "10";
-            when S3 => DESTINO_OUT <= "11";
-            when others => DESTINO_OUT <= "00";
+        case STATE is                     -- Cambia de destino en funcion del estado, que es igual que la planta
+            when S0 =>
+                DESTINO_OUT <= "00";
+            when S1 =>
+                DESTINO_OUT <= "01";
+            when S2 =>
+                DESTINO_OUT <= "10";
+            when S3 =>
+                DESTINO_OUT <= "11";
+            when others =>
+                DESTINO_OUT <= "00";
         end case;
     end process;
     
@@ -46,33 +51,31 @@ begin
     begin
         SIGUIENTE_STATE <= S0;
         case (STATE) is
-        
-          when S0 =>
-              if (DESTINO = "00") then
-                  SIGUIENTE_STATE <= S0;
-                  PUERTA <= "10";
-                  MOTOR <= "00";
-                  FLAG_MOTOR <= '0';
-              elsif (DESTINO /= "00") then
-                  PUERTA <= "01";
-                  MOTOR <= "01";
-                  SIGUIENTE_STATE <= S1;
-                  PISO_TEMP <= DESTINO;
-                  FLAG_MOTOR <= '1';
-              end if;
-                      
-          when S1 =>
-              if (PISO_TEMP /= "01") then 
-                 if(PISO_TEMP = "10") or (PISO_TEMP = "11") then
-                     SIGUIENTE_STATE <= S2;
-                     MOTOR <= "01";
-                 elsif(PISO_TEMP = "00") then
-                     SIGUIENTE_STATE <= S0;
-                     MOTOR <= "10";
-                 end if;
-                 PUERTA <= "01";
-                 FLAG_MOTOR <= '1';
-              else 
+                      when S0 =>                                  -- En el estado 0
+                if DESTINO = "00" then                  -- Si el destino es planta 0
+                    SIGUIENTE_STATE <= S0;              -- Cambia a estado 0
+                    MOTOR           <= "00";            -- Para motor
+                    FLAG_MOTOR      <= '0';             -- Final de movimiento de ascensor
+                    PUERTA          <= "10";            -- Abre puerta
+                elsif DESTINO /= "00" then              -- Si el destino no es planta 0
+                    SIGUIENTE_STATE <= S1;              -- Cambia de estado al siguiente
+                    MOTOR           <= "01";            -- Motor en marcha (subiendo)
+                    FLAG_MOTOR      <= '1';             -- Inicio movimiento de ascensor
+                    PISO_TEMP       <= DESTINO;         -- Cambia de piso al siguiente
+                    PUERTA          <= "01";            -- Cierra puerta
+                end if;
+            when S1 =>                                                  -- En el estado 1
+                if PISO_TEMP /= "01" then                               -- Si la planta no es la 1
+                    if PISO_TEMP = "10" or PISO_TEMP = "11" then        -- Si la planta es la 2 o la 3
+                        SIGUIENTE_STATE <= S2;                          -- Cambia a estado 2
+                        MOTOR           <= "01";                        -- Motor en marcha (subiendo)
+                    elsif PISO_TEMP = "00" then                         -- Si la planta es la 0
+                        SIGUIENTE_STATE <= S0;                          -- Cambia a estado 0
+                        MOTOR           <= "10";                        -- Motor en marcha (bajando)
+                    end if;
+                    PUERTA     <= "01";                                 -- Cierra puerta
+                    FLAG_MOTOR <= '1';                                  -- Inicio movimiento de ascensor
+                else                                                    -- Funcionamiento similar
                  if (DESTINO = "00") then
                      SIGUIENTE_STATE <= S0;
                      PUERTA <= "01";
